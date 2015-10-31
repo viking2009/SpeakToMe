7d829c8f49c21ea67ab0b7a516fc49e785f3849b
Mykola Vyshynskyi <viking2009@ua.fm>
2015-10-31 19:48:53 +0200

    Initial Commit

---
+++ SpeakToMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpeakToMe
+//
+//  Created by Mykola Vyshynskyi on 31.10.15.
+//  Copyright © 2015 Mykola Vyshynskyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SpeakToMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpeakToMe
+//
+//  Created by Mykola Vyshynskyi on 31.10.15.
+//  Copyright © 2015 Mykola Vyshynskyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
